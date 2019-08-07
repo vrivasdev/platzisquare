85450b7e4ea1f07f1a96665ddbc3514e44d5a464
Víctor Rivas <victor.rivas.sistemas@gmail.com>
2019-08-07 13:36:33 -0400

    Firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { AgmCoreModule } from '@agm/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { AppComponent } from './app.component';
 import { LugaresComponent } from './lugares/lugares.component';
@@ -14,6 +17,16 @@ import { ResaltarDirective } from './directives/resaltar.directive';
 import { ContarClicksDirective } from './directives/contar-clicks.directive';
 
 import { LugaresService } from './services/lugares.service';
+
+export const firebaseConfig = {
+  apiKey           : 'AIzaSyDIh5Xvi3Uttw-KaEomPqMEtCTCpCeGglk',
+  authDomain       : 'platzisquare-248923.firebaseapp.com',
+  databaseURL      : 'https: //platzisquare-248923.firebaseio.com',
+  projectId        : 'platzisquare-248923',
+  storageBucket    : '',
+  messagingSenderId: '619450697948',
+  appId            : '1    : 619450697948: web: 3bda0330ff5875c2'
+};
 
 const appRoutes: Routes = [
   {path: '', component       : LugaresComponent},
@@ -37,7 +50,10 @@ const appRoutes: Routes = [
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyDVd7-b7HyS2dWGKoKO0vKeGFOXpAEbtVU'
     }),
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [LugaresService],
   bootstrap: [AppComponent]
